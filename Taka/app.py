c1a3256541d75977466f95dc1ca3e242ea16297f
hsd01 <36105117+hsd01@users.noreply.github.com>
2020-11-07 13:37:59 +0530

    Update app.py

--- Taka/app.py
+++ Taka/app.py
@@ -3,18 +3,21 @@ import sqlite3
 
 app = Flask(__name__)
 
+# this is where http://localhost:5000/ will point
 @app.route('/')
 def home():
 	return render_template('home.html')
 
+# New wntry page this will led to html entry form
 @app.route('/enternew')
 def add_new():
 	return render_template('addnew.html')
 
-
+# this will add the record to data base
 @app.route('/addrec', methods=["POST","GET"])
 def addrec():
 	if request.method == 'POST':
+		# try to get name and other details from form we created earlier
 		try:
 			name = request.form['name']
 			brand = request.form['brand_name']
@@ -25,7 +28,7 @@ def addrec():
 			cl2 = request.form['classification_l2']
 			cl3 = request.form['classification_l3']
 			cl4 = request.form['classification_l4']
-			
+			# connect and open database
 			with sqlite3.connect("db/GDT_1.db") as conn:
 				c = conn.cursor()
 				c.execute("""INSERT INTO GreenDeck 
@@ -42,16 +45,21 @@ def addrec():
 		finally:
 			return render_template("result.html",msg=msg)
 
+# this function will allow us to view list of data in our database "GDT_1.db"
 @app.route('/list')
 def list():
+	# connect and open database
 	conn = sqlite3.connect("db/GDT_1.db")
 	conn.row_factory = sqlite3.Row
 	c = conn.cursor()
-	c.execute("SELECT * FROM GreenDeck")
+	c.execute("SELECT * FROM GreenDeck") # selecting everything from table named GreenDeck
 	rows = c.fetchall()
 	return render_template("list.html",rows=rows)
 
 
+# function to delete the data from database
+# keep in mind to delete data from database user must need admin permision to do so in any database host system
+# 
 @app.route('/delete_item')
 def delete_this():
 	return render_template('del_sub.html')
@@ -75,4 +83,4 @@ def delete():
 		return render_template("result.html",msg=msg)
 
 if __name__=='__main__':
-	app.run(host='0.0.0.0')d
+	app.run(host='0.0.0.0')
